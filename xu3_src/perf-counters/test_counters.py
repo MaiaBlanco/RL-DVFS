068832dd40a4a2baf1f6df07fa1de5d56ec7de80
MarkBlanco <markb1@andrew.cmu.edu>
2018-05-15 21:16:43 +0000

    Reduce number of loops in test script.

--- xu3_src/perf-counters/test_counters.py
+++ xu3_src/perf-counters/test_counters.py
@@ -3,12 +3,15 @@ import time
 
 perf.reset_counters()
 print('Cycles\t\tInstructions')
-for i in range(100):
+for i in range(10):
 	print(perf.cycle_count(), perf.inst_count())
 	time.sleep(0.1)
 
 print("\n\nTesting getting counters over time:")
 
+perf.reset_counters()
 for i in range(10):
 	l = perf.perf_w_period(100);
-	print(l)
+	print("cycles: {}\t\tinstructions: {}\t\t"\
+		"branch misses: {}\t\tDmem Access: {}\t\tl2 refill:{}".format(
+			l[0], l[1], l[2], l[3], l[4]))
