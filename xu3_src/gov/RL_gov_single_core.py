108b0425f54b63bb81abb4e26ffbb8d2d1d20d82
MarkBlanco <markb1@andrew.cmu.edu>
2018-05-26 18:07:29 +0000

    Add state space distribution data and updated script for single-core RL agent.

--- xu3_src/gov/RL_gov_single_core.py
+++ xu3_src/gov/RL_gov_single_core.py
@@ -1,23 +1,28 @@
-import sysfs_paths_xu3 as sfs
-import devfreq_utils_xu3 as dvfs
-from state_space_params_xu3 import *
 import numpy as np
 import multiprocessing as mp
 import subprocess, os
 import ctypes
 import time
 
+# Local imports:
+import sysfs_paths_xu3 as sfs
+import devfreq_utils_xu3 as dvfs
+from state_space_params_xu3 import *
+from state_space_params_xu3 import freq_to_bucket
 
+# Redefine state space number of vars for single-core implementation:
+VARS = 7
 # Array of global state-action values. Dimensions are:
 # (num vf settings for big cluster * num state variables * max num buckets )
 Q = np.zeros( (FREQS, VARS, BUCKETS) ) 
-VARS = 6
 
 def checkpoint_statespace():
-	return None
+	ms_period = int(PERIOD*1000)
+	np.save("statespace_{}ms.npy".format(ms_period))
 
 def load_statespace():
-	return None
+	ms_period = int(PERIOD*1000)
+	return np.load("statespace_{}ms.npy".format(ms_period))
 
 # XU3 has built-in sensors, so use them:
 def get_power():
@@ -34,13 +39,13 @@ def reward1(counters, temps, power):
 	return reward
 
 def init_RL():
-	# Make sure perf counter module is loaded
+	# Make sure perf counter module is loaded:
 	process = subprocess.Popen(['lsmod'], stdout=subprocess.PIPE)
 	output, err = process.communicate()
-	loaded = "perf_counters" in output
+	loaded = "perfmod" in output
 	if not loaded:
 		print("WARNING: perf-counters module not loaded. Loading...")
-		process = subprocess.Popen(['sudo', 'insmod', 'perf-counters.ko'])
+		process = subprocess.Popen(['sudo', 'insmod', 'perfmod.ko'])
 		output, err = process.communicate()
 	profile_statespace() 
 
@@ -54,29 +59,37 @@ def get_counter_value(cpu_num, attr_name):
 	return val
 
 def set_period(p):
-	for cpu_num in range(4,5):
-		with open("/sys/kernel/performance_counters/cpu{}/sample_period_ms".format(cpu_num), 
+	cpu_num = 4
+	with open("/sys/kernel/performance_counters/cpu{}/sample_period_ms".format(cpu_num), 
 				'w') as f:
-			f.write("{}\n".format(p))
+		f.write("{}\n".format(p))
 
 
 '''
 Returns state figures, non-quantized.
-Includes branch misses per Kinstruction, IPC, and data memory accesses per Kinstruction for each core, 
-plus core temp and big cluster power. (BMPKI, IPC, MPKI, celsius, watts)
+Includes branch misses per Kinstruction, IPC, and l2miss, data memory accesses 
+per Kinstruction for each core, plus core temp and big cluster power. 
+(BMPKI, IPC, MPKI, MDMEMAPKI, celsius, watts)
 TODO: add leakage power?
 TODO: add v/f levels?
+TODO: add thermal predictions?
 '''
 def get_raw_state():	
 	# Get the change in counter values:
-	diffs = np.zeros((1,4))
+	diffs = np.zeros((1,5))
 	P = get_power()
-
-	for cpu in range(4,5):
-		diffs[cpu-4, 0] = get_counter_value(cpu, "branch_mispredictions")
-		diffs[cpu-4, 1] = get_counter_value(cpu, "cycles")
-		diffs[cpu-4, 2] = get_counter_value(cpu, "instructions_retired")
-		diffs[cpu-4, 3] = get_counter_value(cpu, "data_memory_accesses")
+	cpu = 4
+		
+	cpu_freq = dvfs.getClusterFreq(cpu)
+	# Convert cpu freq from kHz to Hz and then multiply by period;
+	total_possible_cycles = int(cpu_freq * 1000 * PERIOD)
+	#cycles_used = get_counter_value(cpu, "cycles")
+	
+	diffs[cpu-4, 0] = get_counter_value(cpu, "branch_mispredictions")
+	diffs[cpu-4, 1] = total_possible_cycles
+	diffs[cpu-4, 2] = get_counter_value(cpu, "instructions_retired")
+	diffs[cpu-4, 3] = get_counter_value(cpu, "l2_data_refills")
+	diffs[cpu-4, 4] = get_counter_value(cpu, "data_memory_accesses")
 
 	T = [float(x) for x in dvfs.getTemps()]
 
@@ -85,8 +98,9 @@ def get_raw_state():
 	# Convert cycles to kiloCycles to make IPC computation make sense:
 	diffs[:,1] /= 1000.0
 	# Compute state params from that:
-	# bmiss/Kinst, IPC, dmem_accesses/Kinst, temp, power
-	raw_state = [diffs[0,0]/diffs[0,2], diffs[0,2]/diffs[0,1], diffs[0,3]/diffs[0,2], T[0],P]
+	# bmiss/Kinst, IPC, L2misses/Kinst, dmem_accesses/Kinst, temp, power
+	raw_state = [diffs[0,0]/diffs[0,2], diffs[0,2]/diffs[0,1], diffs[0,3]/diffs[0,2], 
+				 diffs[0,4]/diffs[0,2], T[0],P, cpu_freq]
 	return raw_state
 
 
@@ -95,11 +109,18 @@ Place state in 'bucket' given min/max values and number of buckets for each valu
 '''
 def bucket_state(raw):
 	# Use bucket width to determine index of each raw state value:
-	all_mins = np.array([bmiss_MIN, ipc_MIN, mpi_MIN, temp_MIN]*4 + [pwr_MIN])
-	all_widths = np.array([bmiss_width, ipc_width, mpi_width, temp_width]*4 + [pwr_width])
+	all_mins = np.array([bmiss_MIN, ipc_MIN, mpi_MIN, dmemi_MIN, temp_MIN] \
+															+ [pwr_MIN] + [0])
+	all_widths = np.array([bmiss_width, ipc_width, mpi_width, dmemi_width, temp_width] \
+																+ [pwr_width] + [1])
 	raw_floored = np.array(raw) - all_mins
 	state = np.divide(raw_floored, all_widths)
-	return state
+	state[-1] = freq_to_bucket(raw[-1])
+	for i, x in enumerate(state):
+		if x > (BUCKETS-1):
+			print("WARN: Stat {} has greater bucket than {}: {}".format(i, BUCKETS-1, x))
+			state[i] = BUCKETS-1
+	return [int(x) for x in state]
 
 
 def profile_statespace():
@@ -108,23 +129,35 @@ def profile_statespace():
 	try:
 		max_state = np.load('max_state_{}ms_single_core.npy'.format(ms_period))
 		min_state = np.load('min_state_{}ms_single_core.npy'.format(ms_period))
+		print("Loaded previously checkpointed states.")
 	except:
 		max_state = get_raw_state()
 		min_state = max_state
 	i = 0
+	stat_counts = np.zeros((VARS, BUCKETS), dtype=np.uint64)
 	while True:
 		start = time.time()
 		raw = get_raw_state()
 		max_state = np.maximum.reduce([max_state, raw])
 		min_state = np.minimum.reduce([min_state, raw])
+		bucketed = bucket_state(raw)
+		for stat_index, loc in enumerate(bucketed):
+			stat_counts[stat_index, loc] += 1
+		
 		i += 1
 		if i % 100 == 0:
 			np.save('max_state_{}ms_single_core.npy'.format(ms_period), max_state)
 			np.save('min_state_{}ms_single_core.npy'.format(ms_period), min_state)
+			np.save('bucket_counts_{}ms_single_core.npy'.format(ms_period), stat_counts)
 			print("{}: Checkpointed raw state max and min.".format(i))
 			print(raw)
+			print(bucketed)
+		
 		end = time.time()
-		time.sleep((float(ms_period)/1000) - (end-start))
+		elapsed = end-start
+		if elapsed > PERIOD:
+			print("WARN: elapsed > period ({} > {})".format(elapsed, period)
+		time.sleep(max(0, PERIOD - elapsed))
 
 
 def Q_learning(states):
--- xu3_src/gov/state_space_params_xu3.py
+++ xu3_src/gov/state_space_params_xu3.py
@@ -21,8 +21,8 @@ Estimated big cluster leakage power
 '''
 
 # Dimensions of state space:
-BUCKETS = 15
-VARS = 17
+BUCKETS = 19
+VARS = 18
 FREQS = 19
 # Epsilon
 E = 0.01
@@ -36,37 +36,44 @@ RHO = 0.0
 c4bm = 0 
 c4ipc = 1
 c4mpi = 2
-c4t = 3
+c4dmemapi = 3
 c5bm = 4 
 c5ipc = 5
 c5mpi = 6
-c5t = 7
+c5dmemapi = 7
 c6bm = 8 
 c6ipc = 9
 c6mpi = 10
-c6t = 11
+c6dmemapi = 11
 c7bm = 12 
 c7ipc = 13
 c7mpi = 14
-c7t = 15
+c7dmemapi = 15
 pwr = 16
 lkpwr = 17
 
 # Estimated ranges for each value type:
 bmiss_MIN = 0.0
-bmiss_MAX = 2.0 
+bmiss_MAX = 100.0 
 ipc_MIN = 0.0
 ipc_MAX = 3.0
 mpi_MIN = 0.0
-mpi_MAX = 2.0
+mpi_MAX = 50
+dmemi_MIN = 0.0
+dmemi_MAX = 4000
 temp_MIN = 35.0
-temp_MAX = 75.0
-pwr_MIN = 0.25
+temp_MAX = 80.0
+pwr_MIN = 0.00
 pwr_MAX = 15.0
 
 # Compute width of each bucket for each state dimension:
 bmiss_width = (bmiss_MAX - bmiss_MIN) / BUCKETS
 ipc_width = (ipc_MAX - ipc_MIN) / BUCKETS
 mpi_width = (mpi_MAX - mpi_MIN) / BUCKETS
+dmemi_width = (dmemi_MAX - dmemi_MIN) / BUCKETS
 temp_width = (temp_MAX - temp_MIN) / BUCKETS
 pwr_width = (pwr_MAX - pwr_MIN) / BUCKETS
+
+def freq_to_bucket(freq):
+	big_freqs = [2000000,1900000,1800000,1700000,1600000,1500000,1400000,1300000,1200000,1100000,1000000,900000,800000,700000,600000,500000,400000,300000,200000]	
+	return big_freqs.index(int(freq))
