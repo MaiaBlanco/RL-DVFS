ac8425c88fe9f9352aaa237e123aa1cd95ee8f40
MarkBlanco <markb1@andrew.cmu.edu>
2018-06-03 00:46:34 +0000

    Make fan control script work.

--- xu3_src/gov/fan_control.py
+++ xu3_src/gov/fan_control.py
@@ -1,12 +1,29 @@
-with open('/sys/devices/odroid_fan.14/fan_mode', 'w') as f:
-	f.write("0")
+#! /usr/bin/python
+import atexit
+import sys
 
-with open('/sys/devices/odroid_fan.14/fan_mode', 'w') as f:
-	f.write("1")
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+			if sys.argv[1] == "on":
+				# Set to manual mode:
+				with open('/sys/devices/odroid_fan.14/fan_mode', 'w') as f:
+					f.write("0")
+				# Turn fan all the way on (in manual):
+				with open('/sys/devices/odroid_fan.14/pwm_duty', 'w') as f:
+					f.write("255")	
+			elif sys.argv[1] == "off":
+				# Set to manual mode:
+				with open('/sys/devices/odroid_fan.14/fan_mode', 'w') as f:
+					f.write("0")
+				# Turn fan off (in manual):
+				with open('/sys/devices/odroid_fan.14/pwm_duty', 'w') as f:
+					f.write("1")
+			elif sys.argv[1] == "auto":
+				# set to automatic mode:
+				with open('/sys/devices/odroid_fan.14/fan_mode', 'w') as f:
+					f.write("1")
+	else:
+		print("Invalid option! Select on, off, or auto.")
 
-with open('/sys/devices/odroid_fan.14/pwm_duty', 'w') as f:
-	f.write("1")
 
-with open('/sys/devices/odroid_fan.14/pwm_duty', 'w') as f:
-	f.write("255")	
 
